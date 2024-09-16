465e6a33b305a8246b2494ec80ae8261fe0cb0c1
monkeytrick <35412534+monkeytrick@users.noreply.github.com>
2024-09-16 14:16:54 +0900

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,9 @@ use Symfony\Component\DomCrawler\Crawler;
 
 require 'functions.php';
 
+// Get single quote
 if(isset($_GET['q']) && $_GET['q'] == 'challenge') {
-
     $get_quote = url_call('challenge', $_GET['p']);
-}
-
-else if(isset($_GET['img'])) {
-    get_imgs($_GET['img']);
-    // var_dump("function call params ar e", $_GET['img']);
-    // var_dump($_GET['imgs']);
 }
 
 else {
